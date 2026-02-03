823f67dd59de64b7aa75bf8b6d465b6b600c25b0
VINOTH KUMAR V <vk9360781721@gmail.com>
2026-02-03 08:10:36 +0530

    solved 2073

---
+++ Queue/2073 — Time Needed to Buy Tickets.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/time-needed-to-buy-tickets/
+
+def sol(tickets, k):
+    time = 0
+    for i in range(len(tickets)):
+        if i <= k:
+            time += min(tickets[i], tickets[k])
+        else:
+            time += min(tickets[i],tickets[k]-1)
+    return time
+
+
+print(sol([2,2,2], 2))  # Output: 6
+print(sol([5,1,1,1], 0))  # Output: 8
